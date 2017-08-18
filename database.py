d7e6af84a9195cfa3ca33ec67db6720805a4b8b3
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-08-19 00:26:19 +0200

    Add TelegramUser db table
    Containing all the basic Telegram data

--- database.py
+++ database.py
@@ -9,3 +9,24 @@ Session = sessionmaker(bind=engine)
 
 # Create a new default session
 session = Session()
+
+
+class TelegramUser(Base):
+    """Basic Telegram user data"""
+    __tablename__ = "tusers"
+
+    tid = Column(BigInteger, primary_key=True)
+    tusername = Column(String)
+    tfirstname = Column(String, nullable=False)
+    tlastname = Column(String)
+
+    def __str__(self):
+        if self.tusername is not None:
+            return f"@{self.tusername}"
+        elif self.tlastname is not None:
+            return f"{self.tfirstname} {self.tlastname}"
+        else:
+            return f"{self.tfirstname}"
+
+    def __repr__(self):
+        return f"<User #{self.tid}>"
